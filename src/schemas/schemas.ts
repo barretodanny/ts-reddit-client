9e2508d89f2abf44452fc5cf4c16fe3c6263d322
barretodanny <dannybarreto22@gmail.com>
2023-12-16 17:56:25 -0500

    createUserSchema

---
+++ src/schemas/schemas.ts
@@ -0,0 +1,15 @@
+import { object, string } from "zod";
+
+// USER SCHEMAS
+export const createUserSchema = object({
+  username: string().min(1, "Password is required."),
+  password: string().min(6, "Password must be at least 6 characters."),
+  passwordConfirmation: string().min(
+    6,
+    "Password must be at least 6 characters."
+  ),
+  email: string().min(1, "Email is required.").email("Invalid email."),
+}).refine((data) => data.password === data.passwordConfirmation, {
+  message: "Passwords do not match",
+  path: ["passwordConfirmation"],
+});
