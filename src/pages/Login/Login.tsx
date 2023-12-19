d18a5283c2b506d561d84a5f96f8e2e39aa52a1d
barretodanny <dannybarreto22@gmail.com>
2023-12-19 10:15:10 -0500

    redirect user to home page when visiting /auth/login or /auth/register while already logged in

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getLoggedInUser } from "../../api";
 import LoginForm from "../../components/LoginForm/LoginForm";
 
 function Login() {
-  return (
-    <div>
-      <h3>Login Page</h3>
-      <LoginForm />
-    </div>
-  );
+  const [showPage, setShowPage] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchLoggedInUser = async () => {
+      try {
+        const response = await getLoggedInUser();
+
+        // user is logged in
+        if (response && response.status === 200) {
+          navigate("/");
+        } else {
+          setShowPage(true);
+        }
+      } catch (error) {
+        // error fetching logged in user
+        setShowPage(true);
+      }
+    };
+
+    fetchLoggedInUser();
+  }, []);
+
+  if (showPage) {
+    return (
+      <div>
+        <h3>Login Page</h3>
+        <LoginForm />
+      </div>
+    );
+  }
 }
 
 export default Login;
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getLoggedInUser } from "../../api";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 
 function Register() {
-  return (
-    <div>
-      <h3>Register Page</h3>
-      <RegisterForm />
-    </div>
-  );
+  const [showPage, setShowPage] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchLoggedInUser = async () => {
+      try {
+        const response = await getLoggedInUser();
+
+        // user is logged in
+        if (response && response.status === 200) {
+          navigate("/");
+        } else {
+          setShowPage(true);
+        }
+      } catch (error) {
+        // error fetching logged in user
+        setShowPage(true);
+      }
+    };
+
+    fetchLoggedInUser();
+  }, []);
+
+  if (showPage) {
+    return (
+      <div>
+        <h3>Register Page</h3>
+        <RegisterForm />
+      </div>
+    );
+  }
+
+  return <></>;
 }
 
 export default Register;
