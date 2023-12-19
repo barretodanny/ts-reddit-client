37eab87386643dd53df7d408b98374ba023efc60
barretodanny <dannybarreto22@gmail.com>
2023-12-19 09:44:24 -0500

    POST /api/sessions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,16 @@
 import axios from "axios";
-import { CreateUserInput } from "../types/types";
+import { CreateSessionInput, CreateUserInput } from "../types/types";
 
 const ENDPOINT = "http://localhost:1337/api";
 
 export const createUser = async (userData: CreateUserInput) => {
   const response = await axios.post(`${ENDPOINT}/users`, userData);
+  return response;
+};
+
+export const createSession = async (loginData: CreateSessionInput) => {
+  const response = await axios.post(`${ENDPOINT}/sessions`, loginData, {
+    withCredentials: true,
+  });
   return response;
 };
