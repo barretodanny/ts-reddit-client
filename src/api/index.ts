b4657d75e7a2d7e639e8524a761cd9892bc22aaf
barretodanny <dannybarreto22@gmail.com>
2023-12-19 10:01:45 -0500

    DELETE /api/sessions

--- src/api/index.ts
+++ src/api/index.ts
@@ -15,6 +15,13 @@ export const createSession = async (loginData: CreateSessionInput) => {
   return response;
 };
 
+export const deleteSession = async () => {
+  const response = await axios.delete(`${ENDPOINT}/sessions`, {
+    withCredentials: true,
+  });
+  return response;
+};
+
 export const getLoggedInUser = async () => {
   const response = await axios.get(`${ENDPOINT}/me`, { withCredentials: true });
   return response;
