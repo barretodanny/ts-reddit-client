bca7a456dbd942a12c5e15654433566e26f43a9c
barretodanny <dannybarreto22@gmail.com>
2023-12-19 11:24:18 -0500

    Navbar component styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,13 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { User } from "../../types/types";
 import { getLoggedInUser } from "../../api";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import { BiUserCircle, BiDotsHorizontalRounded } from "react-icons/bi";
 import NavSearchBar from "../NavSearchBar/NavSearchBar";
 import LogoutBtn from "../LogoutBtn/LogoutBtn";
+
+import styles from "./Navbar.module.css";
 
 function Navbar() {
   const [loggedInUser, setLoggedInUser] = useState<User>();
@@ -27,18 +32,38 @@ function Navbar() {
   }, []);
 
   return (
-    <nav>
-      <div>
-        <div>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/subreddits"}>Subreddits</Link>
-          <Link to={"/users"}>Users</Link>
+    <nav className={styles.container}>
+      <div className={styles.left}>
+        <AiOutlineMenu
+          size={25}
+          className={`${styles.moreIcon} ${styles.menuIcon}`}
+        />
+        <div className={styles.mobileLinks}>
+          <Link to={"/"} className={styles.homeLink}>
+            Home
+          </Link>
+          <Link
+            to={"/subreddits"}
+            className={`${styles.homeLink} ${styles.mobileLink}`}
+          >
+            Subreddits
+          </Link>
+          <Link
+            to={"/users"}
+            className={`${styles.homeLink} ${styles.mobileLink}`}
+          >
+            Users
+          </Link>
         </div>
-        <div>
-          <button>Discover</button>
-          <div>
-            <Link to={"/subreddits"}>Subreddits</Link>
-            <Link to={"/users"}>Users</Link>
+        <div className={styles.dropdown}>
+          <button className={styles.dropbtn}>Discover</button>
+          <div className={styles.dropdownContent}>
+            <Link to={"/subreddits"} className={styles.dropdownLink}>
+              Subreddits
+            </Link>
+            <Link to={"/users"} className={styles.dropdownLink}>
+              Users
+            </Link>
           </div>
         </div>
       </div>
@@ -46,31 +71,67 @@ function Navbar() {
       <NavSearchBar />
 
       {loggedInUser ? (
-        <div>
-          <button>
-            <span>{loggedInUser.username}</span>
+        <div className={styles.userDropdown}>
+          <button className={styles.userDropdownBtn}>
+            <BiUserCircle size={30} className={styles.userIcon} />
+            <span className={styles.usernameText}>{loggedInUser.username}</span>
           </button>
-          <div>
-            <Link to={`/u/${loggedInUser.username}`}>View Profile</Link>
-            <Link to={`/u/${loggedInUser.username}/edit`}>Edit Profile</Link>
-            <Link to={"/subreddits/create"}>Create Subreddit</Link>
-            <Link to={`/u/${loggedInUser.username}/subreddits`}>
+          <div className={styles.userDropdownContent}>
+            <Link
+              to={`/u/${loggedInUser.username}`}
+              className={styles.userDropdownOption}
+            >
+              View Profile
+            </Link>
+            <Link
+              to={`/u/${loggedInUser.username}/edit`}
+              className={styles.userDropdownOption}
+            >
+              Edit Profile
+            </Link>
+            <Link
+              to={"/subreddits/create"}
+              className={styles.userDropdownOption}
+            >
+              Create Subreddit
+            </Link>
+            <Link
+              to={`/u/${loggedInUser.username}/subreddits`}
+              className={styles.userDropdownOption}
+            >
               View Your Subreddits
             </Link>
-            <Link to={`/u/${loggedInUser.username}/posts`}>
+            <Link
+              to={`/u/${loggedInUser.username}/posts`}
+              className={styles.userDropdownOption}
+            >
               View Your Posts
             </Link>
-            <Link to={`/u/${loggedInUser.username}/comments`}>
+            <Link
+              to={`/u/${loggedInUser.username}/comments`}
+              className={styles.userDropdownOption}
+            >
               View Your Comments
             </Link>
             <LogoutBtn />
           </div>
         </div>
       ) : (
-        <div>
-          <div>
-            <Link to={"/auth/login"}>Login</Link>
-            <Link to={"/auth/register"}>Sign Up</Link>
+        <div className={styles.auth}>
+          <BiDotsHorizontalRounded size={30} className={styles.moreIcon} />
+          <div className={styles.authBtns}>
+            <Link
+              to={"/auth/login"}
+              className={`${styles.authBtn} ${styles.login}`}
+            >
+              Login
+            </Link>
+            <Link
+              to={"/auth/register"}
+              className={`${styles.authBtn} ${styles.signup}`}
+            >
+              Sign Up
+            </Link>
           </div>
         </div>
       )}
