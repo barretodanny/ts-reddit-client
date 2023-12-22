769588f129472b85369c20abcd95a93711b70e1f
barretodanny <dannybarreto22@gmail.com>
2023-12-21 20:14:59 -0500

    subreddits/create page and styling

--- src/pages/CreateSubreddit/CreateSubreddit.tsx
+++ src/pages/CreateSubreddit/CreateSubreddit.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { User } from "../../types/types";
+import { getLoggedInUser } from "../../api";
+import SubredditForm from "../../components/SubredditForm/SubredditForm";
+
+import styles from "./CreateSubreddit.module.css";
 
 function CreateSubreddit() {
-  return <div>CreateSubreddit</div>;
+  const [loggedInUser, setLoggedInUser] = useState<User>();
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    document.title = "Create New Subreddit";
+
+    const fetchLoggedInUser = async () => {
+      try {
+        const response = await getLoggedInUser();
+
+        // user is logged in
+        if (response && response.status === 200) {
+          setLoggedInUser(response.data);
+        }
+      } catch (error) {
+        // error fetching logged in user
+      }
+      setShowContent(true);
+    };
+
+    fetchLoggedInUser();
+  }, []);
+
+  if (!showContent) {
+    return <></>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <Link to={"/subreddits"} className={styles.headingLink}>
+        <h1 className={`${styles.heading} ${styles.white} ${styles.center}`}>
+          Subreddits
+        </h1>
+      </Link>
+      {!loggedInUser ? (
+        <p className={`${styles.text} ${styles.white} ${styles.center}`}>
+          Must be{" "}
+          <Link to={"/auth/login"} className={styles.loginLink}>
+            logged in
+          </Link>{" "}
+          to create a Subreddit
+        </p>
+      ) : (
+        <SubredditForm />
+      )}
+    </div>
+  );
 }
 
 export default CreateSubreddit;
