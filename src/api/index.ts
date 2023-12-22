e4eda478bd46006de6a335e42b595542fc7e563f
barretodanny <dannybarreto22@gmail.com>
2023-12-22 09:57:59 -0500

    GET /api/users

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,6 +8,11 @@ import {
 const ENDPOINT = "http://localhost:1337/api";
 
 // USERS
+export const getUsers = async (searchParams: string) => {
+  const response = await axios.get(`${ENDPOINT}/users${searchParams}`);
+  return response;
+};
+
 export const createUser = async (userData: CreateUserInput) => {
   const response = await axios.post(`${ENDPOINT}/users`, userData);
   return response;
