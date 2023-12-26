de9c9b6fbdd173b5caef6591eddde35a80265df0
barretodanny <dannybarreto22@gmail.com>
2023-12-26 18:58:31 -0500

    CommentsWrapper component and styling

---
+++ src/components/CommentsWrapper/CommentsWrapper.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { Comment, Post, User } from "../../types/types";
+import { useLocation } from "react-router-dom";
+import { getPostComments } from "../../api";
+
+import CommentList from "../CommentList/CommentList";
+import CreateCommentForm from "../CreateCommentForm/CreateCommentForm";
+
+import styles from "./CommentsWrapper.module.css";
+
+interface CommentsWrapperProps {
+  post: Post;
+  loggedInUser: User | undefined;
+}
+
+function CommentsWrapper({ post, loggedInUser }: CommentsWrapperProps) {
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [newComments, setNewComments] = useState<Comment[]>([]);
+
+  const location = useLocation();
+  const searchParams = location.search;
+
+  useEffect(() => {
+    setNewComments([]);
+
+    getPostComments(post._id, searchParams).then((data) => {
+      setComments(data.data);
+    });
+  }, [post._id, searchParams]);
+
+  return (
+    <div className={styles.container}>
+      {comments.length < 1 && newComments.length < 1 && (
+        <h2 className={`${styles.heading} ${styles.mt45}`}>
+          Nobody has commented on this post
+        </h2>
+      )}
+      {loggedInUser && (
+        <CreateCommentForm
+          post={post}
+          loggedInUser={loggedInUser}
+          // @ts-ignore
+          setComments={setComments}
+        />
+      )}
+      {/* Render the newly created comments first if any */}
+      <CommentList
+        comments={newComments}
+        loggedInUser={loggedInUser}
+        depth={0}
+        fetched={false}
+      />
+      {/* Render the fetched comments */}
+      <CommentList
+        comments={comments}
+        loggedInUser={loggedInUser}
+        depth={0}
+        fetched={true}
+      />
+    </div>
+  );
+}
+
+export default CommentsWrapper;
