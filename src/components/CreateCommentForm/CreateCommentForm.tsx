f29b92b62be84401d931e93c19e03b15215b7ac1
barretodanny <dannybarreto22@gmail.com>
2023-12-26 18:56:48 -0500

    CreateCommentForm component and stylhing

---
+++ src/components/CreateCommentForm/CreateCommentForm.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { CreateCommentInput, Post, User } from "../../types/types";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createCommentSchema } from "../../schemas/schemas";
+import { Link } from "react-router-dom";
+
+import styles from "./CreateCommentForm.module.css";
+import { createComment } from "../../api";
+
+interface CreateCommentFormProps {
+  post: Post;
+  loggedInUser: User;
+  setComments: React.Dispatch<React.SetStateAction<Comment[]>>;
+}
+
+function CreateCommentForm({
+  post,
+  loggedInUser,
+  setComments,
+}: CreateCommentFormProps) {
+  const [createError, setCreateError] = useState("");
+  const { register, handleSubmit, reset } = useForm<CreateCommentInput>({
+    resolver: zodResolver(createCommentSchema),
+  });
+
+  async function onSubmit(values: CreateCommentInput) {
+    try {
+      // create comment
+      const response = await createComment(post._id, values);
+      const comment = response.data;
+      setCreateError("");
+      setComments((prev) => [
+        { ...comment, user: { _id: comment.user } },
+        ...prev,
+      ]);
+      reset({ content: "" });
+    } catch (error: any) {
+      Array.isArray(error.response.data)
+        ? setCreateError(error.response.data[0].message)
+        : setCreateError(error.response.data.message);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <p className={`${styles.m0} ${styles.grey}`}>
+        Comment as{" "}
+        <Link to={`/u/${loggedInUser.username}`} className={styles.link}>
+          {loggedInUser.username}
+        </Link>
+      </p>
+      {createError && <p className={styles.warning}>{createError}</p>}
+      <div className={styles.formWrapper}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.formElement}>
+            <textarea
+              id="content"
+              {...register("content")}
+              placeholder="Comment here..."
+              className={styles.fieldTextArea}
+            />
+          </div>
+          <button type="submit" className={styles.submit}>
+            SUBMIT
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CreateCommentForm;
