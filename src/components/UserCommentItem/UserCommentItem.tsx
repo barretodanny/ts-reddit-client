dcf9adcb4e2337b2605ab60ce922c3098daf2a29
barretodanny <dannybarreto22@gmail.com>
2023-12-27 09:37:57 -0500

    UserCommentItem component and styling

---
+++ src/components/UserCommentItem/UserCommentItem.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { Comment, Subreddit } from "../../types/types";
+import { getTimeAgo } from "../../utils/utils";
+import { getSubredditById } from "../../api";
+import { Link } from "react-router-dom";
+
+import styles from "./UserCommentItem.module.css";
+
+interface UserCommentItemProps {
+  comment: Comment;
+}
+
+function UserCommentItem({ comment }: UserCommentItemProps) {
+  const [subreddit, setSubreddit] = useState<Subreddit>();
+
+  const timeAgo = getTimeAgo(comment.createdAt);
+
+  useEffect(() => {
+    const fetchSubreddit = async () => {
+      try {
+        // @ts-ignore
+        const response = await getSubredditById(comment.post.subreddit);
+        setSubreddit(response);
+      } catch (error: any) {
+        // error fetching subreddit
+      }
+    };
+
+    fetchSubreddit();
+  }, []);
+
+  if (!subreddit) {
+    return <></>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <span className={styles.postedBy}>Posted by </span>
+        <Link to={`/u/${comment.user.username}`} className={styles.userLink}>
+          {comment.user.username}
+        </Link>
+        <span className={styles.time}>{timeAgo}</span>
+        <span className={styles.points}>(Points: {comment.points})</span>
+      </div>
+      <div className={styles.contentContainer}>
+        <p className={styles.content}>{comment.content}</p>
+      </div>
+      <div className={styles.bottom}>
+        <span className={styles.commentedOn}>Commented on: </span>
+        <Link
+          to={`/r/${subreddit.name}/post/${comment.post._id}`}
+          className={`${styles.postLink} ${styles.blueLink}`}
+        >
+          {comment.post.title}
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default UserCommentItem;
