b15bbeaeeba9ba47e9aeb6e9053dacd764b1f528
barretodanny <dannybarreto22@gmail.com>
2023-12-27 13:59:11 -0500

    DeleteAccountBtn component and styling

---
+++ src/components/DeleteAccountBtn/DeleteAccountBtn.tsx
@@ -0,0 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { User } from "../../types/types";
+
+import styles from "./DeleteAccountBtn.module.css";
+import { deleteSession, deleteUser } from "../../api";
+
+interface DeleteAccountBtnProps {
+  user: User;
+}
+
+function DeleteAccountBtn({ user }: DeleteAccountBtnProps) {
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    const result = window.confirm(
+      "Are you sure you want to delete your account?"
+    );
+
+    if (result) {
+      deleteUser(user._id);
+      await deleteSession();
+      navigate("/");
+      navigate(0);
+    }
+  };
+
+  return (
+    <button type="button" onClick={handleClick} className={styles.deleteBtn}>
+      DELETE USER
+    </button>
+  );
+}
+
+export default DeleteAccountBtn;
