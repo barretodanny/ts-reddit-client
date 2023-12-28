48248eada0ceb229131115d6f272cd8c4ee103d6
barretodanny <dannybarreto22@gmail.com>
2023-12-28 11:06:50 -0500

    use redux state to track logged in user

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,54 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getLoggedInUser } from "../../api";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../redux/store";
+import { getAuthUser, reset } from "../../redux/slices/AuthSlice";
 import LoginForm from "../../components/LoginForm/LoginForm";
 
 import styles from "./Login.module.css";
 
 function Login() {
-  const [showPage, setShowPage] = useState(false);
-
   const navigate = useNavigate();
+  const dispatch: AppDispatch = useDispatch();
+  const { loggedInUser, isSuccess, isLoading } = useSelector(
+    (state: RootState) => state.auth
+  );
 
   useEffect(() => {
     document.title = "Sign In";
 
-    const fetchLoggedInUser = async () => {
-      try {
-        const response = await getLoggedInUser();
-
-        // user is logged in
-        if (response && response.status === 200) {
-          navigate("/");
-        } else {
-          setShowPage(true);
-        }
-      } catch (error) {
-        // error fetching logged in user
-        setShowPage(true);
-      }
-    };
-
-    fetchLoggedInUser();
+    dispatch(getAuthUser());
   }, []);
 
-  if (showPage) {
-    return (
-      <div className={styles.container}>
-        <h1 className={`${styles.text} ${styles.heading}`}>Welcome!</h1>
-        <h3 className={`${styles.text} ${styles.subHeading}`}>
-          Please Sign in to your account
-        </h3>
-        <LoginForm />
-        <p className={styles.text}>
-          Don&apos;t have an account?{" "}
-          <Link to={"/auth/register"} className={styles.registerText}>
-            Sign Up
-          </Link>
-        </p>
-      </div>
-    );
+  useEffect(() => {
+    if (loggedInUser) {
+      navigate("/");
+      dispatch(reset());
+    }
+  }, [loggedInUser]);
+
+  if (loggedInUser || isLoading || !isSuccess) {
+    return <></>;
   }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={`${styles.text} ${styles.heading}`}>Welcome!</h1>
+      <h3 className={`${styles.text} ${styles.subHeading}`}>
+        Please Sign in to your account
+      </h3>
+      <LoginForm />
+      <p className={styles.text}>
+        Don&apos;t have an account?{" "}
+        <Link to={"/auth/register"} className={styles.registerText}>
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,54 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getLoggedInUser } from "../../api";
+import { AppDispatch, RootState } from "../../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { getAuthUser, reset } from "../../redux/slices/AuthSlice";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 
 import styles from "./Register.module.css";
 
 function Register() {
-  const [showPage, setShowPage] = useState(false);
-
   const navigate = useNavigate();
+  const dispatch: AppDispatch = useDispatch();
+  const { loggedInUser, isSuccess, isLoading } = useSelector(
+    (state: RootState) => state.auth
+  );
 
   useEffect(() => {
     document.title = "Sign Up";
 
-    const fetchLoggedInUser = async () => {
-      try {
-        const response = await getLoggedInUser();
-
-        // user is logged in
-        if (response && response.status === 200) {
-          navigate("/");
-        } else {
-          setShowPage(true);
-        }
-      } catch (error) {
-        // error fetching logged in user
-        setShowPage(true);
-      }
-    };
-
-    fetchLoggedInUser();
+    dispatch(getAuthUser());
   }, []);
 
-  if (showPage) {
-    return (
-      <div className={styles.container}>
-        <h1 className={`${styles.text} ${styles.heading}`}>Welcome!</h1>
-        <h3 className={`${styles.text} ${styles.subHeading}`}>
-          Create an account
-        </h3>
-        <RegisterForm />
-        <p className={styles.text}>
-          Already have an account?{" "}
-          <Link to={"/auth/login"} className={styles.loginText}>
-            Login
-          </Link>
-        </p>
-      </div>
-    );
+  useEffect(() => {
+    if (loggedInUser) {
+      navigate("/");
+      dispatch(reset());
+    }
+  }, [loggedInUser]);
+
+  if (loggedInUser || isLoading || !isSuccess) {
+    return <></>;
   }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={`${styles.text} ${styles.heading}`}>Welcome!</h1>
+      <h3 className={`${styles.text} ${styles.subHeading}`}>
+        Create an account
+      </h3>
+      <RegisterForm />
+      <p className={styles.text}>
+        Already have an account?{" "}
+        <Link to={"/auth/login"} className={styles.loginText}>
+          Login
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default Register;
